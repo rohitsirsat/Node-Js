34f27b89d189b63b028d7115d32ff8a0c4fb3785
rohit <rohitsirsat855@gmail.com>
2025-12-21 23:16:06 +0530

    Refactor script.js to comment out file reading operations and enhance Blob example

--- 01_node/script.js
+++ 01_node/script.js
@@ -2,7 +2,7 @@ const fs = require("node:fs");
 
 // Task: read the content of notes.txt file
 
-console.log("Start fo Script");
+// console.log("Start fo Script");
 
 // Sync ==> Blocking operation
 // const content = fs.readFileSync("notes.txt", "utf-8");
@@ -10,9 +10,23 @@ console.log("Start fo Script");
 // console.log("Content got: ", content);
 
 // [Async] => non-blocking code
-fs.readFile("notes.txt", "utf-8", function (error, data) {
-  if (error) console.log(error);
-  else console.log("Content got: ", data);
-});
+// fs.readFile("notes.txt", "utf-8", function (error, data) {
+//   if (error) console.log(error);
+//   else console.log("Content got: ", data);
+// });
 
-console.log("End of Script");
+// console.log("End of Script");
+
+/**
+ *@description: Blob example in Node.js (read more about Blob)
+
+ * const obj = { hello: "world" };
+// const blob = new Blob([JSON.stringify(obj, null, 2)], {
+//   type: "application/json",
+// });
+
+const blob = new Blob([JSON.stringify(obj, null, 2)]);
+
+console.log("heee: ", blob);
+ * 
+ */
